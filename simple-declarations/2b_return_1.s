c373b121d76c612af622eaaa494ccbfa366d515d
Chad Estioco <chadestioco@gmail.com>
2018-08-01 00:28:12 +0800

    More experiments.

---
+++ simple-declarations/2b_return_1.s
@@ -0,0 +1,22 @@
+# Exactly the same as 2a_return_0 except for line 14.
+	.file	"2b_return_1.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$1, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.10) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
