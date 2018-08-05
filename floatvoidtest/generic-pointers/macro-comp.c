1e26c1e471264ea2c2fada9c13f58a2a9a8384cf
Chad Estioco <chadestioco@gmail.com>
2018-08-06 03:41:41 +0800

    Just a bit more clarifications.

--- floatvoidtest/generic-pointers/macro-comp.c
+++ floatvoidtest/generic-pointers/macro-comp.c
@@ -3,6 +3,7 @@
 #define ptrind(bp, item_size, index) bp + item_size * index
 
 void foo(void *arr, size_t item_size){
+    printf("Raw arg address %p.\n", arr);
     printf("The array starts at %p.\n", &arr);
     printf("Address of index 1: %p.\n", (&arr) + item_size * 1); 
     printf("(macro) &address of index 1: %p.\n", ptrind(&arr, item_size, 1));
@@ -11,5 +12,9 @@ void foo(void *arr, size_t item_size){
 
 int main(){
     int bar[] = {3, 1, 4, 1};
+    printf("int has size %ld.\n", sizeof(int));
+    printf("(main) The array starts at %p.\n", &bar);
+    printf("(main) index 0 is at %p.\n", &(bar[0]));
+    printf("(main) index 1 is at %p.\n", &(bar[1]));
     foo(&bar, sizeof(int));
 }
