e30c2f22bbab12a6040e0644decf66b2ddeb770e
Chad Estioco <chadestioco@gmail.com>
2018-08-05 22:29:03 +0800

    This illustrates my problem perfectly.

--- floatvoidtest/generic-pointers/macro-comp.c
+++ floatvoidtest/generic-pointers/macro-comp.c
@@ -5,7 +5,8 @@
 void foo(void *arr, size_t item_size){
     printf("The array starts at %p.\n", &arr);
     printf("Address of index 1: %p.\n", (&arr) + item_size * 1); 
-    printf("(macro) address of index 1: %p.\n", ptrind(&arr, item_size, 1));
+    printf("(macro) &address of index 1: %p.\n", ptrind(&arr, item_size, 1));
+    printf("(macro) *address of index 1: %p.\n", ptrind(arr, item_size, 1));
 }
 
 int main(){
